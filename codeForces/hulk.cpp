12c2247a5f4ea2275de2cec5c9dbd2377d5a9233
Mehran Kamal <mkbalochhoth@gmail.com>
2019-05-30 14:18:03 +0500

    Solved Hulk(CodeForces)

---
+++ codeForces/hulk.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+
+    if(n == 1)
+    {
+        cout << "I hate ";
+    }
+    else
+    {        
+        for (int i=1; i<=n; i++)
+        {
+            if(i%2 == 0)
+            {
+                cout << "I love ";
+            }
+            else
+            {
+                cout << "I hate ";
+            }
+            if(i!=n)
+            {
+                cout << "that ";
+            }
+        }
+    }
+    
+    cout << "it" << endl;
+
+    return 0;
+}
