d22e3238c29b5323b7c1bd82734cbfed3cbe6b1f
Mehran Kamal <mkbalochhoth@gmail.com>
2019-06-20 19:06:56 +0500

    Solved Rails (UVa Online Judge)

---
+++ UVa-Online-Judge/rails.cpp
@@ -0,0 +1,40 @@
+//Problem:          
+//Problem Link:     
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    while(true)
+    {
+        int n, coach = 1;
+        cin >> n;
+        if(n==0)    break;
+        while(true)
+        {
+            stack<int> station;
+            int inp;
+            coach = 0;
+            bool can = true;
+            cin >> inp;
+            if(inp == 0)    break;
+            for(int i=0; i<n; i++)
+            {
+                if(i!=0)    cin >> inp;
+                while(inp > coach)
+                    station.push(++coach);
+                if(station.empty() || station.top() != inp)
+                    can = false;
+                else
+                    station.pop();
+            }
+            if(can) cout << "Yes" << endl;
+            else    cout << "No" << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
