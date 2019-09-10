656881e0cd6e70de0921ddd02a6949aa8d773bf3
Mehran Kamal <mkbalochhoth@gmail.com>
2019-09-10 22:08:28 +0500

    Solved Celebrity Jeopardy (UVa Online Judge)

---
+++ UVa-Online-Judge/celebrity-jeopardy.cpp
@@ -0,0 +1,16 @@
+//Problem:          Celebrity Jeopardy
+//Problem Link:     https://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=3565
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    char inp[1000000];
+    int c = 0;
+    while(cin.getline(inp, 100000))
+        cout << inp << endl;
+
+    return 0;
+}
