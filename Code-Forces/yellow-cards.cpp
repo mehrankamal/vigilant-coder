7d0240fb320c14a9cb11a37d5adb359071a69307
Mehran Kamal <mkbalochhoth@gmail.com>
2019-09-28 14:07:00 +0500

    Yellow Cards (Code Forces) solved.

--- Code-Forces/yellow-cards.cpp
+++ Code-Forces/yellow-cards.cpp
@@ -17,22 +17,17 @@ int main()
     int a1, a2, k1, k2, n;
 
     cin >> a1 >> a2 >> k1 >> k2 >> n;
-
+    int minn, maxx = 0;
+    if(k1 > k2)
     {
-        int minn, maxx = 0;
-        if(k1 > k2)
-        {
-            swap(k1, k2);
-            swap(a1, a2);
-        }
-        int remaining = (a1 * (k1 - 1)) + (a2 * (k2 -1));
-        minn = max(n-remaining, 0);
-        if(n <= (a1 * k1)) maxx = floor(n/k1);
-        else maxx = a1 + floor((n-a1*k1)/k2);
-        cout << minn << " " << maxx << endl;
+        swap(k1, k2);
+        swap(a1, a2);
     }
-    
-
+    int remaining = (a1 * (k1 - 1)) + (a2 * (k2 -1));
+    minn = max(n-remaining, 0);
+    if(n <= (a1 * k1)) maxx = floor(n/k1);
+    else maxx = a1 + floor((n-a1*k1)/k2);
+    cout << minn << " " << maxx << endl;
 
     return 0;
 }
