1b90e80c3844fc1020036d182b8d2ce0209c1c5b
Mehran Kamal <mkbalochhoth@gmail.com>
2019-10-10 20:28:43 +0500

    Hello World! (Kattis) solved

---
+++ Kattis/hello-world.cpp
@@ -0,0 +1,22 @@
+//Problem:          Hello World!
+//Problem Link:     https://open.kattis.com/problems/hello
+
+#include<bits/stdc++.h>
+#define vi vector<int>
+#define ll long long
+#define pi pair<int, int>
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define rep(i, a, b) for(int i=a; i<=b; i++)
+#define repn(i, n) for(int i=0; i<(int)n; i++)
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    return 0;
+}
