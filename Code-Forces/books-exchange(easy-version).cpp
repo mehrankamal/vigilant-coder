5a5978712552cc0a72a2cbeea79f11e73f28ec48
Mehran Kamal <mkbalochhoth@gmail.com>
2019-10-23 05:52:26 +0500

    Books Exchange (easy version) (Code Forces) solved.

---
+++ Code-Forces/books-exchange(easy-version).cpp
@@ -0,0 +1,45 @@
+//Problem:          
+//Problem Link:     
+
+#include<bits/stdc++.h>
+#define vi vector<int>
+#define ll long long
+#define pi pair<int, int>
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define rep(i, a, b) for(int i=a; i<=b; i++)
+#define repn(i, n) for(int i=0; i<(int)n; i++)
+
+using namespace std;
+
+int main()
+{
+    int q;
+    cin >> q;
+    while(q--)
+    {
+        int n;
+        cin >> n;
+        vi arr(n);
+        repn(i, n)
+            cin >> arr[i];
+        repn(i, n)
+        {
+            if(i > 0)   cout << " ";
+            int pos = arr[i] - 1;
+            int ans = 1;
+            while(pos != i)
+            {
+                ans += 1;
+                pos = arr[pos] - 1;
+            }
+            cout << ans;
+        }
+        cout << endl;
+    }
+    
+
+    return 0;
+}
