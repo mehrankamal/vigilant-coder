1803e4cafdfa12bc9eaecbe1cb98c09c272be6a9
Mehran Kamal <mkbalochhoth@gmail.com>
2019-10-25 01:15:59 +0500

    k-rounding (Code Forces) solved.

---
+++ Code-Forces/k-rounding.cpp
@@ -0,0 +1,30 @@
+//Problem:          
+//Problem Link:     
+
+#include<bits/stdc++.h>
+#define vi vector<int>
+#define ll long long
+#define pi pair<int, int>
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+#define rep(i, a, b) for(int i=a; i<=b; i++)
+#define repn(i, n) for(int i=0; i<(int)n; i++)
+
+using namespace std;
+
+int main()
+{
+    ll n, k;
+    cin >> n >> k;
+    ll ans = n * pow(10, k);
+    ans = ans / __gcd(n, (ll)ceil(pow(10.0, k)));
+
+    if(ans%10 != 0 && k != 0) ans += 1;
+
+    cout << ans << endl;
+
+
+    return 0;
+}
